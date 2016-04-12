c2d73ec926bf5475594aeff9c94b4e8eca8d1713
Prasanth <galithotty.prasanth@stayzilla.com>
2016-04-12 16:01:53 +0530

    Prasanth: Include android project files

---
+++ app/src/main/java/io/darter/cms/darterclasscom/MainActivity.java
@@ -0,0 +1,13 @@
+package io.darter.cms.darterclasscom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
